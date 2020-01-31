f1090f5fac17abcfe3d56ea13b65e5e1e1c15abe
inventivetalent <mail@inventivetalent.org>
2020-02-01 00:37:43 +0100

    don't use utc

--- index.php
+++ index.php
@@ -268,6 +268,12 @@
                 }
             ];
 
+            Highcharts.setOptions({
+                global: {
+                    useUTC: false
+                }
+            });
+
 
             $(document).ready(() => {
 
